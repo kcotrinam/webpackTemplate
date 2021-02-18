47b525d5763a6ce6d7c787d0cd6f01dfed529f95
Kevin Cotrina <kcotrinam@gmail.com>
2021-02-18 18:47:08 -0500

    Add svg to image compiler

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
         type: 'asset/resource',
       },
     ]
