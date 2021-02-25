bdbc596ad5bbace1188c2eb590147e1c69ed733d
kevin Cotrina <50785733+kcotrinam@users.noreply.github.com>
2021-02-25 09:44:01 -0500

    Fix relative path

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     filename: "app.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    publicPath: "./",
     assetModuleFilename: 'images/[name][ext][query]'
   },
   plugins: [
