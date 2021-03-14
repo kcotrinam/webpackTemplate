c4d14138936219019ef9f24ad489389cc71a8d7f
Kevin Cotrina <kcotrinam92@gmail.com>
2021-03-14 00:32:21 -0500

    Add components folder Delete async/await example

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
 import "./scss/styles.scss"
-
-const requestJoke = async () => {
-  const url = "https://api.chucknorris.io/jokes/random";
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-}
-
-const displayJoke = async () => {
-  const joke = await requestJoke();
-  console.log(joke.value)
-}
-
-displayJoke()
