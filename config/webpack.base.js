60e0e8e2b7b230e7fe0aa20518779fec6067def1
Kevin Cotrina <kcotrinam92@gmail.com>
2021-03-17 12:24:39 -0500

    Add config folder.

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "app.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../dist"),
     publicPath: "./",
     assetModuleFilename: 'images/[name][ext][query]'
   },
